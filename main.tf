8fd0e6f370b4cbc61006806669150e9cb8925ef7
Ilya Kohanovskiy <i.kohanvs@gmail.com>
2022-03-17 17:34:47 +0400

    remove target group, security group, lb listener resourcer (pass them from props)

--- main.tf
+++ main.tf
@@ -11,25 +11,6 @@ resource "aws_ecr_repository" "app_repo" {
   }
 }
 
-resource "aws_lb_target_group" "app_tg" {
-  name        = "${local.aws_alb_target_group_name}"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-}
-
-resource "aws_lb_listener" "app_listener" {
-  load_balancer_arn = var.load_balancer_id
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = aws_lb_target_group.app_tg.id
-    type             = "forward"
-  }
-}
-
 resource "aws_ecs_task_definition" "app_task_def" {
   family                   = "${local.aws_ecs_service_name}"
   network_mode             = "awsvpc"
@@ -59,25 +40,6 @@ resource "aws_ecs_task_definition" "app_task_def" {
     DEFINITION
 }
 
-resource "aws_security_group" "app_sg" {
-  name        = "${local.aws_ecs_service_security_group_name}"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    protocol        = "tcp"
-    from_port       = 80
-    to_port         = 80
-    security_groups = [var.load_balancer_security_group_id]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_ecs_service" "app_ecs_service" {
   name            = "${local.aws_ecs_service_name}"
   cluster         = var.cluster_id
@@ -86,16 +48,16 @@ resource "aws_ecs_service" "app_ecs_service" {
   desired_count   = 1
 
   network_configuration {
-    security_groups = [aws_security_group.app_sg.id]
+    security_groups = [var.security_group_id]
     subnets         = var.private_subnets
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.app_tg.id
+    target_group_arn = var.target_group_id
     container_name   = "${local.aws_ecs_service_name}"
     container_port   = 80
   }
 
-  depends_on = [aws_lb_listener.app_listener]
+  depends_on = [var.lb_listener]
 }
 
--- variables.tf
+++ variables.tf
@@ -2,8 +2,8 @@ variable "aws_resource_name_prefix" {
   description = "Prefix to be used in the naming of some of the created AWS resources e.g. demo-webapp"
 } 
 
-variable "vpc_id" {}
-variable "load_balancer_id" {}
-variable "load_balancer_security_group_id" {}
 variable "cluster_id" {}
 variable "private_subnets" {}
+variable "target_group_id" {}
+variable "lb_listener" {}
+variable "security_group_id" {}
